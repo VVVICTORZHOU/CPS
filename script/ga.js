2648ad720964b55e3b296118eef1a8e251fb370d
Victor ZHOU <100321010+VVVICTORZHOU@users.noreply.github.com>
2025-11-09 23:57:06 +0800

    Add files via upload

---
+++ script/ga.js
@@ -0,0 +1,14 @@
+// ga.js - GA4 追蹤碼
+(function() {
+    var gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-M2NJ10LNRZ";
+    document.head.appendChild(gtagScript);
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag; // 對外暴露
+    gtag('js', new Date());
+    gtag('config', 'G-M2NJ10LNRZ');
+  })();
+  
